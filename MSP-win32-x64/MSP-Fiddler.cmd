02d4a5b9d1b3b957f2c49ccd0e685fefd369b1ec
Oskar Kurasz <160154293+oskiielo@users.noreply.github.com>
2026-05-09 16:51:59 +0200

    Disable broken Fiddler proxy launcher

--- MSP-win32-x64/MSP-Fiddler.cmd
+++ MSP-win32-x64/MSP-Fiddler.cmd
@@ -1,7 +1,5 @@
 @echo off
 setlocal
 set MSP_DEBUG=1
-set MSP_FIDDLER=1
-set MSP_FIDDLER_PROXY=127.0.0.1:8888
-set MSP_FIDDLER_BASE_URL=http://127.0.0.1
-start "" "%~dp0MSP-Debug.exe" --debug --fiddler
+set MSP_FIDDLER=0
+start "" "%~dp0MSP-Debug.exe" --debug
